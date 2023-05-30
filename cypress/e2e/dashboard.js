ceda886debfb72964cbba4c5accbecd687e4a2fb
AmirAutomation <amirr10cc@gmail.com>
2023-05-29 22:13:47 -0500

    update

--- cypress/e2e/dashboard.js
+++ cypress/e2e/dashboard.js
@@ -4,5 +4,8 @@ https://docs.sorry-cypress.dev/guide/get-started
 
 npm install cy2 cypress
 
+Plugins para recording  para obtener los identificadores de las paginas
 
+
+Cyprress Scenario recorder google chrome 
 */
